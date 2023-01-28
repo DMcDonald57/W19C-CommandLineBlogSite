20a6b89150a6d1b9a4a96a9fbeee11bb4afdc20e
DMcDonald57 <d.mcdonald579@gmail.com>
2023-01-28 16:11:20 -0700

    Created DB, py files, creds and ignore

---
+++ app.py
@@ -0,0 +1,11 @@
+import mariadb
+import dbcreds
+
+conn = mariadb.connect(
+            user=dbcreds.user,
+            password=dbcreds.password,
+            host=dbcreds.host,
+            port=dbcreds.port,
+            database=dbcreds.database)
+
+cursor = conn.cursor()
