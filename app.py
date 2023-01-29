c011385d97c3edc6ed9bd9330baa75bee5972815
DMcDonald57 <d.mcdonald579@gmail.com>
2023-01-29 16:35:17 -0700

    Cant get the id printed.  Time to reset and try again

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ conn = mariadb.connect(
             database=dbcreds.database)
 
 cursor = conn.cursor()
+
+print("Welcome Back.\n")
+user_name = input("Please enter your User Name\n")
+pass_word = input("Please enter your Password\n")
+
+def login():
+    cursor.execute("SELECT id FROM client WHERE username = %s AND password = %s")
+
+login()
+
